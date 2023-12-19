0bb5de3baa7e5bd9b0d61e6fc7f2279ee0bf25ae
lyr-2000 <lyr-2000@users.noreply.github.com>
2023-12-19 15:58:01 +0000

    Merge branch 'master' of github.com:lyr-2000/a-leetcode-algo-code-note lyr-2000/a-leetcode-algo-code-note@7c58c14ea5dbef8505448a383d1bc6cce1032d29

--- code/421.数组中两个数的最大异或值.cpp
+++ code/421.数组中两个数的最大异或值.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <vector >
+#include <algorithm>
 using namespace std;
 
 /*
@@ -54,15 +56,75 @@ using namespace std;
 // @lc code=start
 class Node {
 public:
-    Node *left;
-    Node* right;
+    Node *v0;
+    Node* v1;
 };
 
+// int 有31位，第32为符号位， 0为开始，所以减去1
+#define hi (31-1)
 class Solution
 {
 public:
+    Node *makeTree(Node *root,int w) {
+        if(!root) return NULL;
+        // if (w==0) return root;
+        for (int k=hi;k>=0;k--) {
+            int b = (w>>k) & 1;
+            if(b) {
+                if(!root->v1)root->v1 = new Node();
+                root = root->v1;
+            }else {
+                if(!root->v0) root->v0 = new Node();
+                root = root->v0;
+            }
+        }
+        return root;
+
+    }
+ 
+    int xorTree(Node *p,int x) {
+        int ans=0;
+        for(int i=hi;i>=0;i--) {
+            int b = (x>>i) & 1;
+            if(b) {
+                // 1 
+                if(p->v0) {
+                    ans = ans *2 +1;
+                    p = p->v0;
+                }else if(p->v1) {
+                   ans = ans *2;
+                    p = p->v1;
+                }
+
+            }else {
+                // 0
+                if(p->v1) {
+                    ans = ans *2 +1;
+                    p = p->v1;
+                }else{
+                    p = p->v0;
+                    ans = ans *2;
+                }
+            }
+        }
+        return ans;
+         
+    }
     int findMaximumXOR(vector<int> &nums)
     {
+        Node p = {NULL,NULL};
+       
+        int ans=0;
+        // 经可能为1
+        for(int i=1,n= nums.size();i<n;i++) {
+            // xor是对称的， a^b = b^a ,这样只需要遍历1遍即可
+            makeTree(&p,nums[i-1]);
+            ans = max(ans,xorTree(&p,nums[i]));
+        }
+        // 0 ^ 0 =>0
+        // 0 ^ 1 => 1
+        // 1 ^ 1 => 0
+        return ans;
     }
 };
 // @lc code=end
@@ -71,7 +133,7 @@ int main()
 {
     cout << " * [421] 数组中两个数的最大异或值" << endl;
     Solution k;
-    auto ans = {1, 2, 3, 3, 5};
+    vector<int> ans = { 0,8 };
 
     cout << "result:  " << k.findMaximumXOR(ans) << endl;
     return 0;
